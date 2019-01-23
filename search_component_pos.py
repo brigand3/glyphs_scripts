abed55d7cde57cf34881b6b7cc6b71e6127ba736
Hyunjin <hjyun@sandoll.co.kr>
2019-01-23 21:17:43 +0900

    Search component position from selected glyphs

--- search_component_pos.py
+++ search_component_pos.py
@@ -7,14 +7,11 @@ Glyphs script for searching component position from selected glyphs
 # Date : 2019/01/15
 
 componentSearch = '_part.LC_Giyeok1'
-if len(Glyphs.font.selectedLayers) <= 1:
-	for glyph in Glyphs.font.glyphs:
-		for layer in glyph.layers:
-			for component in layer.components:
-				if component.componentName == componentSearch:
-					print 'Glyph:%s, X:%.1f, Y:%.1f' % (layer.parent.name, component.position.x, component.position.y)
-else:
-	for layer in Glyphs.font.selectedLayers:
-		for component in layer.components:
-			if component.componentName == componentSearch:
-				print 'Glyph:%s, X:%.1f, Y:%.1f' % (layer.parent.name, component.position.x, component.position.y)
+print '**********************************************************************************'
+print 'Seaching the position of component %s' % layer.parent
+print '**********************************************************************************'
+
+for layer in Glyphs.font.selectedLayers:
+	for component in layer.components:
+		if component.componentName == componentSearch:
+			print 'Glyph:%s, X:%.1f, Y:%.1f' % (layer.parent.name, component.position.x, component.position.y)
